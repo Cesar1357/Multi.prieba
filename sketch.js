8ece0ddd37516175e158214a9c3186ea43e4a69f
Cesar1357 <85760661+Cesar1357@users.noreply.github.com>
2021-09-04 12:23:42 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -18,16 +18,16 @@ function draw(){
 
     
     if(keyDown(LEFT_ARROW)){
-        writePosition(-1,0);
+        writePosition(-2,0);
     }
     else if(keyDown(RIGHT_ARROW)){
-        writePosition(1,0);
+        writePosition(2,0);
     }
     else if(keyDown(UP_ARROW)){
-        writePosition(0,-1);
+        writePosition(0,-2);
     }
     else if(keyDown(DOWN_ARROW)){
-        writePosition(0,+1);
+        writePosition(0,+2);
     }
     drawSprites();
 }
